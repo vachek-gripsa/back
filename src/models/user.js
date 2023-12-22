03ea6350b36ea78d8dc32b5e0dc50209bb23242a
Kostiantyn Sukhykh <konstantin.suhih@gmail.com>
2023-12-22 13:41:24 +0200

    gripsa: add models for mongo

---
+++ src/models/user.js
@@ -0,0 +1,58 @@
+import { Schema, model } from 'mongoose';
+
+const userSchema = new Schema({
+  password: {
+    type: String,
+    required: true
+  },
+  firstName: {
+    type: String,
+    required: true
+  },
+  lastName: {
+    type: String,
+    required: true
+  },
+  userName: {
+    type: String
+  },
+  avatar: {
+    type: String
+  },
+  contacts: [
+    {
+      email: {
+        type: String,
+        required: true
+      }
+    },
+    {
+      phoneNumber: {
+        type: String
+      }
+    },
+    {
+      telegram: {
+        type: String
+      }
+    },
+    {
+      linkedin: {
+        type: String
+      }
+    },
+    {
+      github: {
+        type: String
+      }
+    }
+  ],
+  financialProjects: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: 'FinancialProject'
+    }
+  ]
+});
+
+export const User = model('User', userSchema);
